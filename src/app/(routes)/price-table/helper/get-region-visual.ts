f9c0d5fd769679f9615af1d2000e92aeb2628175
adhi <adzkiyadzikrullah@gmail.com>
2025-11-20 19:24:32 +0700

    fix: types error

--- src/app/(routes)/price-table/helper/get-region-visual.ts
+++ src/app/(routes)/price-table/helper/get-region-visual.ts
@@ -1,9 +1,8 @@
 import { changePriceRegion, priceRegionVisualConfig, priceTypeRegion } from '@/data/price';
 import { PriceType } from '@/types/price';
+import { RegionLayoutId } from '@/types/region';
 
-import { RegionId } from './region-layout';
-
-export const getRegionData = (commodityId: string, tab: PriceType, regionId: RegionId) => {
+export const getRegionData = (commodityId: string, tab: PriceType, regionId: RegionLayoutId) => {
     const source = tab === 'level-harga' ? priceTypeRegion : changePriceRegion;
 
     const group = source.find((g) => g.commodityId === commodityId && g.infoPriceTypeId === tab);
@@ -11,7 +10,7 @@ export const getRegionData = (commodityId: string, tab: PriceType, regionId: Reg
     return group?.values.find((v) => v.id === regionId);
 };
 
-export const getRegionVisual = (commodityId: string, tab: PriceType, regionId: RegionId) => {
+export const getRegionVisual = (commodityId: string, tab: PriceType, regionId: RegionLayoutId) => {
     const regionData = getRegionData(commodityId, tab, regionId);
     const visualFromConfig = priceRegionVisualConfig[commodityId]?.[tab]?.[regionId];
 
